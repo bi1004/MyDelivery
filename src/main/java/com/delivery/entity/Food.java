cf684c66b3b496e479c75d1ddb58bb7a7c43f621
bi1004 <dandy01997@gmail.com>
2023-04-13 13:04:36 +0900

    Food Entity

---
+++ src/main/java/com/delivery/entity/Food.java
@@ -0,0 +1,37 @@
+package com.delivery.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Food {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO.IDENTITY)
+    private Long id; //음식 id,pk
+
+    @Column(nullable = false)
+    private String name; //음식 이름
+
+    @Column(nullable = false)
+    private int price;// 음식 가격
+
+    @Column(nullable = false)
+    private String category; // 음식 카테고리
+
+    @Column(nullable = false)
+    private String description; // 음식 설명
+
+    @Column(nullable = false)
+    private String imageUrl; // 음식 사진 url
+
+    @Column(nullable = false)
+    private int quantity; // 음식 수량
+}
